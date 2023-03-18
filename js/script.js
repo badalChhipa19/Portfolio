4e8d61e20f3f1f70895ca522610c08d8a30d3aaa
Badal <badalchhipa9@gmail.com>
2023-03-18 11:12:20 +0530

    completed till skills, logo fixed, go to top completed

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const headerLists = document.querySelector(".header__lists");
 const headergGreet = document.querySelectorAll(".header__greets");
 const section = document.querySelectorAll(".section");
 const skillGraph = document.querySelectorAll(".skill__graph");
+const header = document.querySelector(".header");
+const headerNav = document.querySelector(".header__nav");
+const goToTop = document.querySelector(".up__arr");
 
 // Link highlite style___
 const heandalLinks = function (e) {
@@ -75,3 +78,35 @@ section.forEach((e) => {
   sectionReveal.observe(e);
   e.classList.add("section__hidden");
 });
+
+// NAVIGATIONS SHOWING
+const navHeight = headerNav.getBoundingClientRect().height;
+const headerFun = function (entries) {
+  let [entery] = entries;
+  if (!entery.isIntersecting) {
+    // NAVBAR
+    headerNav.style.position = "fixed";
+    // headerNav.style.top = "2rem";
+    // headerNav.style.left = "2rem";
+    headerNav.style.width = "100%";
+    headerNav.style.zIndex = "2";
+
+    // GO TO TOP
+    goToTop.style.transition = "all .3s";
+    goToTop.style.bottom = "40%";
+    goToTop.style.zIndex = 2;
+  } else {
+    headerNav.style.position = "relative";
+    goToTop.style.right = "5.7%";
+    goToTop.style.bottom = "0%";
+    goToTop.style.zIndex = -1;
+  }
+};
+
+const headerObserver = new IntersectionObserver(headerFun, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
